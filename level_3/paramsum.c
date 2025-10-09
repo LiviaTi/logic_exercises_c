d36391460e4c2db4a1ff2aaf69f89faa545cde5c
Livia Ferreira Dos Santos <liferrei@c3r5p3.42sp.org.br>
2025-10-09 19:01:49 -0300

    Livia

---
+++ level_3/paramsum.c
@@ -0,0 +1,56 @@
+/*
+Assignment name  : paramsum
+Expected files   : paramsum.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that displays the number of arguments passed to it, followed by
+a newline.
+
+If there are no arguments, just display a 0 followed by a newline.
+
+Example:
+
+$>./paramsum 1 2 3 5 7 24
+6
+$>./paramsum 6 12 24 | cat -e
+3$
+$>./paramsum | cat -e
+0$
+$>
+*/
+
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_putnbr(int n)
+{
+	if (n >= 10)
+		ft_putnbr( n / 10);
+	ft_putchar((n % 10) + '0');
+}
+
+int main (int argc, char *argv[])
+{
+	int num;
+	char *arg;
+	int   i;
+
+	num = 0;
+	i = 0;
+	if (argc > 1)
+	{
+		num = argc - 1;
+		ft_putnbr(num);
+	}
+	else
+	{
+		write(1,"0",1);
+	}
+	write(1, "\n", 1);
+	return (0);
+}
