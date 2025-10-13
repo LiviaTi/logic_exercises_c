6914dedc153f60a058e3236c6160601fa8154d1b
Livia Ferreira dos Santos <liferrei@student.42sp.org.br>
2025-10-13 15:22:33 -0300

    Livia

---
+++ level_2/do_op.c
@@ -0,0 +1,78 @@
+/*
+	Assignment name  : do_op
+	Expected files   : *.c, *.h
+	Allowed functions: atoi, printf, write
+	--------------------------------------------------------------------------------
+
+	Write a program that takes three strings:
+	- The first and the third one are representations of base-10 signed integers
+	that fit in an int.
+	- The second one is an arithmetic operator chosen from: + - * / %
+
+	The program must display the result of the requested arithmetic operation,
+	followed by a newline. If the number of parameters is not 3, the program
+	just displays a newline.
+
+	You can assume the string have no mistakes or extraneous characters. Negative
+	numbers, in input or output, will have one and only one leading '-'. The
+	result of the operation fits in an int.
+
+	Examples:
+
+	$> ./do_op "123" "*" 456 | cat -e
+	56088$
+	$> ./do_op "9828" "/" 234 | cat -e
+	42$
+	$> ./do_op "1" "+" "-43" | cat -e
+	-42$
+	$> ./do_op | cat -e
+	$
+*/
+
+#include <stdlib.h>
+#include <unistd.h>
+
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+void	ft_putnbr(int n)
+{
+	if (n >= 10)
+		ft_putnbr(n / 10);
+	ft_putchar((n % 10) - '0');
+}
+
+int main (int argc, char *argv[])
+{
+	int num_1;
+	int	num_2;
+	char	*operation;
+	int result;
+
+	num_1 = 0;
+	num_2 = 0;
+	if (argc == 4)
+	{
+		num_1 = atoi(argv[1]);
+		num_2 = atoi(argv[3]);
+		operation = argv[2];
+
+		if (operation == '*')
+			result = num_1 * num_2;
+		else if (operation == "/")
+			result = num_1 / num_2;
+		else if (operation == '+')
+			result = num_1 + num_2;
+		else if (operation == '-')
+			result = num_1 - num_2;
+		else if (operation == '%')
+			result = num_1 % num_2;
+		
+		
+	}
+	write (1, "\n", 1);
+	return (0);
+}
