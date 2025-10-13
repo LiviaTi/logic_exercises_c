0f0afcb774307e237a667a73af96b313fdbd9f34
Livia Ferreira dos Santos <liferrei@student.42sp.org.br>
2025-10-13 15:33:14 -0300

    Livia

---
+++ level_2/ft_strcmp.c
@@ -0,0 +1,28 @@
+/*
+	Assignment name  : ft_strcmp
+	Expected files   : ft_strcmp.c
+	Allowed functions:
+	--------------------------------------------------------------------------------
+
+	Reproduce the behavior of the function strcmp (man strcmp).
+
+	Your function must be declared as follows:
+
+	int    ft_strcmp(char *s1, char *s2);
+*/
+
+int		ft_strcmp(char *s1, char *s2)
+{
+	int		i;
+
+	i = 0;
+	while (s1[i])
+	{
+		if (s1[i] < s2[i])
+			return(-1);
+		if (s1[i] > s2[i])
+			return(1);
+		i++;
+	}
+	return (0);
+}
