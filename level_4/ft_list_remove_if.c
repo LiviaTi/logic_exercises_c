2a368f1f8cd12c281217be382cf28d0d6699bc7b
Livia Ferreira Dos Santos <liferrei@student.42sp.org.br>
2025-10-17 21:33:56 -0300

    Livia

--- level_4/ft_list_remove_if.c
+++ level_4/ft_list_remove_if.c
@@ -25,35 +25,29 @@ typedef struct      s_list
 $>
 */
 
-#include "ft_list.h"
 
-void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
+typedef struct      s_list
 {
-	t_list	*tmp;
-	t_list	*prev;
-	t_list	*curr;
+    struct s_list   *next;
+    void            *data;
+}                   t_list;
 
-	if (!begin_list || !*begin_list)
-		return (0);
-	
-	curr = *begin_list;
-	prev = NULL;
-	while(curr)
+#include "ft_list.h"
+#include "stdlib.h"
+
+void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
+{
+	t_list *tmp;
+
+	if ((*begin_list == NULL) || (begin_list == NULL))
+		return ;
+	if (cmp((*begin_list)->data, data_ref) == 0x0)
 	{
-		if ((*cmp)(curr->data, data_ref) == 0)
-		{
-			tmp = curr;
-			if (prev)
-				prev->next = curr->next;
-			else
-				*begin_list = curr->next;
-			curr = curr->next;
-			free(tmp);
-		}
-		else
-		{
-			prev = curr;
-			curr = curr->next;
-		}
+		tmp = *begin_list;
+		*begin_list = (*begin_list)->next;
+		free(tmp);
+		ft_list_remove_if(begin_list, data_ref, cmp);
 	}
+	else
+		ft_list_remove_if(&((*begin_list)->next), data_ref, cmp);
 }
